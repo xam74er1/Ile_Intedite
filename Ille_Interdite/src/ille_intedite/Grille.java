d1467a18a81e690594f54b7e1d1c9729a912b0ae
Alex <Alex@LAPTOP-5ND2SDH9>
2018-06-01 13:20:26 +0200

    Debut initialisation tuiles

--- Ille_Interdite/src/ille_intedite/Grille.java
+++ Ille_Interdite/src/ille_intedite/Grille.java
@@ -7,6 +7,30 @@ import javax.swing.JButton;
 public class Grille {
         IHM ihm;
 	private HashMap<String,Tuile> tuilesListe;
+	private final String[] nomTuiles = ["Le Pont des Abimes",
+	                                    "La Porte de Bronze",
+	                                    "La Caverne des Ombres",
+	                                    "La Porte de Fer",
+	                                    "La Porte d’Or",
+	                                    "Les Falaises de l’Oubli",
+	                                    "Le Palais de Corail",
+	                                    "La Porte d’Argent",
+	                                    "Les Dunes de l’Illusion",
+	                                    "Heliport",
+	                                    "La Porte de Cuivre",
+	                                    "Le Jardin des Hurlements",
+	                                    "La Foret Pourpre",
+	                                    "Le Lagon Perdu",
+	                                    "Le Marais Brumeux",
+	                                    "Observatoire",
+	                                    "Le Rocher Fantome",
+	                                    "La Caverne du Brasier",
+	                                    "Le Temple du Soleil",
+	                                    "Le Temple de La Lune",
+	                                    "Le Palais des Marees",
+	                                    "Le Val du Crepuscule",
+	                                    "La Tour du Guet",
+	                                    "Le Jardin des Murmures"]
         
      
         
