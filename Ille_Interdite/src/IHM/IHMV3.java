2719cad4b569b5d688601a54d1704baa1944297b
Zunkusem <jeremi04@hotmail.fr>
2018-06-19 09:14:03 +0200

    Changement des nom IHM3 + Déplacement curseur

--- Ille_Interdite/src/IHM/IHMV3.java
+++ Ille_Interdite/src/IHM/IHMV3.java
@@ -60,52 +60,226 @@ public class IHMV3 {
 		frame.getContentPane().add(panel, BorderLayout.SOUTH);
 		panel.setLayout(new GridLayout(0, 6, 0, 0));
 		
-		JPanel Sud1 = new JPanel();
-		Sud1.setBackground(new Color(160, 82, 45));
-		FlowLayout fl_Sud1 = (FlowLayout) Sud1.getLayout();
-		fl_Sud1.setVgap(1);
-		fl_Sud1.setHgap(2);
-		panel.add(Sud1);
+		JPanel south1 = new JPanel();
+		south1.setBackground(new Color(160, 82, 45));
+		FlowLayout fl_south1 = (FlowLayout) south1.getLayout();
+		fl_south1.setVgap(1);
+		fl_south1.setHgap(2);
+		panel.add(south1);
 		
-		JPanel panel_15 = new JPanel();
-		Sud1.add(panel_15);
-		panel_15.setBackground(new Color(160, 82, 45));
+		JPanel south11 = new JPanel();
+		south1.add(south11);
+		south11.setBackground(new Color(160, 82, 45));
 		
-		JButton btnNewButton_2 = new JButton("D\u00E9placer");
-		panel_15.add(btnNewButton_2);
+		JButton btDeplacer = new JButton("Deplacer");
+		south11.add(btDeplacer);
 		
-		JPanel panel_14 = new JPanel();
-		panel_14.setBackground(new Color(160, 82, 45));
-		panel.add(panel_14);
+		JPanel south2 = new JPanel();
+		south2.setBackground(new Color(160, 82, 45));
+		panel.add(south2);
 		
-		JButton btnNewButton_3 = new JButton("Donner Carte");
-		panel_14.add(btnNewButton_3);
-		btnNewButton_3.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		JButton btDonnerCarte = new JButton("Donner Carte");
+		south2.add(btDonnerCarte);
+		btDonnerCarte.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		
-		JPanel panel_13 = new JPanel();
-		panel_13.setBackground(new Color(160, 82, 45));
-		panel.add(panel_13);
+		JPanel south3 = new JPanel();
+		south3.setBackground(new Color(160, 82, 45));
+		panel.add(south3);
 		
-		JButton btnNewButton_1 = new JButton("Ass\u00E9cher");
-		panel_13.add(btnNewButton_1);
+		JButton btAssecher = new JButton("Assecher");
+		south3.add(btAssecher);
 		
-		JPanel panel_16 = new JPanel();
-		panel_16.setBackground(new Color(160, 82, 45));
-		panel.add(panel_16);
+		JPanel south4 = new JPanel();
+		south4.setBackground(new Color(160, 82, 45));
+		panel.add(south4);
 		
-		JPanel panel_17 = new JPanel();
-		panel_17.setBackground(new Color(160, 82, 45));
-		panel.add(panel_17);
+		JPanel south5 = new JPanel();
+		south5.setBackground(new Color(160, 82, 45));
+		panel.add(south5);
 		
-		JButton btnNewButton = new JButton("Fin de Tour");
-		panel.add(btnNewButton);
+		JButton btFinDeTour = new JButton("Fin de Tour");
+		panel.add(btFinDeTour);
 		
-		JPanel EAST = new JPanel();
-		EAST.setBackground(new Color(0, 0, 139));
-		frame.getContentPane().add(EAST, BorderLayout.WEST);
-		EAST.setLayout(new GridLayout(0, 1, 0, 0));
+		JPanel northP1 = new JPanel();
+		frame.getContentPane().add(northP1, BorderLayout.NORTH);
+		northP1.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JPanel northP2 = new JPanel();
+		northP2.setBackground(Color.YELLOW);
+		northP1.add(northP2);
+		northP2.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JPanel northP3 = new JPanel();
+		northP2.add(northP3);
+		northP3.setLayout(new GridLayout(0, 4, 0, 0));
+		
+		JPanel northP4 = new JPanel();
+		northP4.setBackground(new Color(139, 69, 19));
+		northP3.add(northP4);
+		
+		JPanel northP41 = new JPanel();
+		northP41.setBackground(new Color(255, 215, 0));
+		northP4.add(northP41);
+		
+		JLabel labelImgTresor1 = new JLabel("New label");
+		northP41.add(labelImgTresor1);
+		
+		JPanel northP5 = new JPanel();
+		northP5.setBackground(new Color(139, 69, 19));
+		northP3.add(northP5);
+		
+		JPanel northP51 = new JPanel();
+		northP5.add(northP51);
+		northP51.setBackground(new Color(255, 215, 0));
+		
+		JLabel labelImgTresor2 = new JLabel("New label");
+		northP51.add(labelImgTresor2);
+		
+		JPanel northP6 = new JPanel();
+		northP6.setBackground(new Color(139, 69, 19));
+		northP3.add(northP6);
+		
+		JPanel northP61 = new JPanel();
+		northP61.setBackground(new Color(255, 215, 0));
+		northP6.add(northP61);
+		
+		JLabel labelImgTresor3 = new JLabel("New label");
+		northP61.add(labelImgTresor3);
+		
+		JPanel northP7 = new JPanel();
+		northP7.setBackground(new Color(139, 69, 19));
+		northP3.add(northP7);
+		
+		JPanel northP71 = new JPanel();
+		northP71.setBackground(new Color(255, 215, 0));
+		northP7.add(northP71);
+		
+		JLabel labelImgTresor4 = new JLabel("New label");
+		northP71.add(labelImgTresor4);
+		
+		JPanel northP8 = new JPanel();
+		northP1.add(northP8);
+		northP8.setLayout(new GridLayout(0, 3, 0, 0));
+		
+		JPanel northP81 = new JPanel();
+		northP81.setBackground(new Color(139, 69, 19));
+		northP8.add(northP81);
+		
+		JPanel northP82 = new JPanel();
+		northP82.setBackground(new Color(139, 69, 19));
+		northP8.add(northP82);
+		
+		JButton btAide = new JButton("Aide");
+		btAide.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		northP8.add(btAide);
+		
+		JPanel CenterP1 = new JPanel();
+		frame.getContentPane().add(CenterP1, BorderLayout.CENTER);
+		CenterP1.setLayout(new BorderLayout(0, 0));
+		
+		JPanel CenterSouthP0 = new JPanel();
+		CenterP1.add(CenterSouthP0, BorderLayout.SOUTH);
+		CenterSouthP0.setLayout(new GridLayout(0, 7, 0, 0));
+		
+		JPanel CenterSouthP1 = new JPanel();
+		CenterSouthP1.setBackground(new Color(173, 216, 230));
+		CenterSouthP0.add(CenterSouthP1);
+		
+		JLabel labelImageJcourant = new JLabel("New label");
+		CenterSouthP1.add(labelImageJcourant);
+		
+		JPanel CenterSouthP2 = new JPanel();
+		CenterSouthP2.setBackground(new Color(173, 216, 230));
+		CenterSouthP0.add(CenterSouthP2);
+		
+		JPanel CenterSouthP21 = new JPanel();
+		CenterSouthP2.add(CenterSouthP21);
+		
+		JLabel labelCarte1 = new JLabel("New label");
+		CenterSouthP21.add(labelCarte1);
+		
+		JPanel CenterSouthP3 = new JPanel();
+		CenterSouthP3.setBackground(new Color(173, 216, 230));
+		CenterSouthP0.add(CenterSouthP3);
+		
+		JPanel CenterSouthP31 = new JPanel();
+		CenterSouthP3.add(CenterSouthP31);
+		
+		JLabel labelCarte2 = new JLabel("New label");
+		CenterSouthP31.add(labelCarte2);
+		
+		JPanel CenterSouthP4 = new JPanel();
+		CenterSouthP4.setBackground(new Color(173, 216, 230));
+		CenterSouthP0.add(CenterSouthP4);
+		
+		JPanel CenterSouthP41 = new JPanel();
+		CenterSouthP4.add(CenterSouthP41);
+		
+		JLabel labelCarte3 = new JLabel("New label");
+		CenterSouthP41.add(labelCarte3);
+		
+		JPanel CenterSouthP5 = new JPanel();
+		CenterSouthP5.setBackground(new Color(173, 216, 230));
+		CenterSouthP0.add(CenterSouthP5);
+		
+		JPanel CenterSouthP51 = new JPanel();
+		CenterSouthP5.add(CenterSouthP51);
+		
+		JLabel labelCarte4 = new JLabel("New label");
+		CenterSouthP51.add(labelCarte4);
+		
+		JPanel CenterSouthP6 = new JPanel();
+		CenterSouthP6.setBackground(new Color(173, 216, 230));
+		CenterSouthP0.add(CenterSouthP6);
+		
+		JPanel CenterSouthP61 = new JPanel();
+		CenterSouthP6.add(CenterSouthP61);
+		
+		JLabel labelCarte5 = new JLabel("New label");
+		CenterSouthP61.add(labelCarte5);
+		
+		JPanel CenterSouthP7 = new JPanel();
+		CenterSouthP7.setBackground(new Color(173, 216, 230));
+		CenterSouthP0.add(CenterSouthP7);
+		
+		JPanel CenterEastP0 = new JPanel();
+		CenterP1.add(CenterEastP0, BorderLayout.EAST);
+		CenterEastP0.setLayout(new GridLayout(3, 1, 0, 0));
+		
+		JPanel CenterEastP1 = new JPanel();
+		CenterEastP1.setBackground(new Color(160, 82, 45));
+		CenterEastP0.add(CenterEastP1);
+		
+		JLabel labelImgJoueur4 = new JLabel("New label");
+		labelImgJoueur4.setHorizontalAlignment(SwingConstants.CENTER);
+		CenterEastP1.add(labelImgJoueur4);
+		
+		JPanel CenterEastP2 = new JPanel();
+		CenterEastP2.setBackground(new Color(160, 82, 45));
+		CenterEastP0.add(CenterEastP2);
+		
+		JLabel labelImgJoueur3 = new JLabel("New label");
+		labelImgJoueur3.setHorizontalAlignment(SwingConstants.CENTER);
+		CenterEastP2.add(labelImgJoueur3);
+		
+		JPanel CenterEastP3 = new JPanel();
+		CenterEastP3.setBackground(new Color(160, 82, 45));
+		CenterEastP0.add(CenterEastP3);
+		
+		JLabel labelImgJoueur2 = new JLabel("New label");
+		labelImgJoueur2.setHorizontalAlignment(SwingConstants.CENTER);
+		CenterEastP3.add(labelImgJoueur2);
+		
+		JPanel CenterWestP0 = new JPanel();
+		CenterWestP0.setBackground(new Color(0, 0, 139));
+		CenterP1.add(CenterWestP0, BorderLayout.CENTER);
+		CenterWestP0.setLayout(new BorderLayout(0, 0));
 		
 		JSlider slider = new JSlider();
+		CenterWestP0.add(slider, BorderLayout.WEST);
 		slider.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
 		slider.setForeground(new Color(250, 235, 215));
 		slider.setPaintLabels(true);
@@ -115,188 +289,6 @@ public class IHMV3 {
 		slider.setMinorTickSpacing(1);
 		slider.setMaximum(10);
 		slider.setOrientation(SwingConstants.VERTICAL);
-		EAST.add(slider);
-		
-		JPanel panel_1 = new JPanel();
-		frame.getContentPane().add(panel_1, BorderLayout.NORTH);
-		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
-		
-		JPanel panel_2 = new JPanel();
-		panel_1.add(panel_2);
-		panel_2.setLayout(new GridLayout(0, 1, 0, 0));
-		
-		JPanel panel_4 = new JPanel();
-		panel_2.add(panel_4);
-		panel_4.setLayout(new GridLayout(0, 4, 0, 0));
-		
-		JPanel panel_5 = new JPanel();
-		panel_5.setBackground(new Color(139, 69, 19));
-		panel_4.add(panel_5);
-		
-		JPanel panel_7 = new JPanel();
-		panel_7.setBackground(new Color(255, 215, 0));
-		panel_5.add(panel_7);
-		
-		JLabel lblNewLabel_5 = new JLabel("New label");
-		panel_7.add(lblNewLabel_5);
-		
-		JPanel panel_6 = new JPanel();
-		panel_6.setBackground(new Color(139, 69, 19));
-		panel_4.add(panel_6);
-		
-		JPanel panel_9 = new JPanel();
-		panel_6.add(panel_9);
-		panel_9.setBackground(new Color(255, 215, 0));
-		
-		JLabel lblNewLabel_7 = new JLabel("New label");
-		panel_9.add(lblNewLabel_7);
-		
-		JPanel Carte4 = new JPanel();
-		Carte4.setBackground(new Color(139, 69, 19));
-		panel_4.add(Carte4);
-		
-		JPanel panel_21 = new JPanel();
-		panel_21.setBackground(new Color(255, 215, 0));
-		Carte4.add(panel_21);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		panel_21.add(lblNewLabel);
-		
-		JPanel panel_19 = new JPanel();
-		panel_19.setBackground(new Color(139, 69, 19));
-		panel_4.add(panel_19);
-		
-		JPanel panel_20 = new JPanel();
-		panel_20.setBackground(new Color(255, 215, 0));
-		panel_19.add(panel_20);
-		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		panel_20.add(lblNewLabel_1);
-		
-		JPanel panel_3 = new JPanel();
-		panel_1.add(panel_3);
-		panel_3.setLayout(new GridLayout(0, 3, 0, 0));
-		
-		JPanel panel_11 = new JPanel();
-		panel_11.setBackground(new Color(139, 69, 19));
-		panel_3.add(panel_11);
-		
-		JPanel panel_12 = new JPanel();
-		panel_12.setBackground(new Color(139, 69, 19));
-		panel_3.add(panel_12);
-		
-		JButton btnNewButton_4 = new JButton("Aide");
-		btnNewButton_4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		panel_3.add(btnNewButton_4);
-		
-		JPanel panel_18 = new JPanel();
-		frame.getContentPane().add(panel_18, BorderLayout.EAST);
-		panel_18.setLayout(new GridLayout(3, 1, 0, 0));
-		
-		JPanel panel_8 = new JPanel();
-		frame.getContentPane().add(panel_8, BorderLayout.CENTER);
-		panel_8.setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel_10 = new JPanel();
-		panel_8.add(panel_10, BorderLayout.SOUTH);
-		panel_10.setLayout(new GridLayout(0, 7, 0, 0));
-		
-		JPanel panel_52 = new JPanel();
-		panel_52.setBackground(new Color(173, 216, 230));
-		panel_10.add(panel_52);
-		
-		JLabel lblNewLabel_12 = new JLabel("New label");
-		panel_52.add(lblNewLabel_12);
-		
-		JPanel panel_51 = new JPanel();
-		panel_51.setBackground(new Color(173, 216, 230));
-		panel_10.add(panel_51);
-		
-		JPanel panel_53 = new JPanel();
-		panel_51.add(panel_53);
-		
-		JLabel lblNewLabel_6 = new JLabel("New label");
-		panel_53.add(lblNewLabel_6);
-		
-		JPanel panel_50 = new JPanel();
-		panel_50.setBackground(new Color(173, 216, 230));
-		panel_10.add(panel_50);
-		
-		JPanel panel_54 = new JPanel();
-		panel_50.add(panel_54);
-		
-		JLabel lblNewLabel_8 = new JLabel("New label");
-		panel_54.add(lblNewLabel_8);
-		
-		JPanel panel_49 = new JPanel();
-		panel_49.setBackground(new Color(173, 216, 230));
-		panel_10.add(panel_49);
-		
-		JPanel panel_55 = new JPanel();
-		panel_49.add(panel_55);
-		
-		JLabel lblNewLabel_9 = new JLabel("New label");
-		panel_55.add(lblNewLabel_9);
-		
-		JPanel panel_48 = new JPanel();
-		panel_48.setBackground(new Color(173, 216, 230));
-		panel_10.add(panel_48);
-		
-		JPanel panel_56 = new JPanel();
-		panel_48.add(panel_56);
-		
-		JLabel lblNewLabel_10 = new JLabel("New label");
-		panel_56.add(lblNewLabel_10);
-		
-		JPanel panel_46 = new JPanel();
-		panel_46.setBackground(new Color(173, 216, 230));
-		panel_10.add(panel_46);
-		
-		JPanel panel_57 = new JPanel();
-		panel_46.add(panel_57);
-		
-		JLabel lblNewLabel_11 = new JLabel("New label");
-		panel_57.add(lblNewLabel_11);
-		
-		JPanel panel_47 = new JPanel();
-		panel_47.setBackground(new Color(173, 216, 230));
-		panel_10.add(panel_47);
-		
-		JPanel panel_22 = new JPanel();
-		panel_8.add(panel_22, BorderLayout.EAST);
-		panel_22.setLayout(new GridLayout(3, 1, 0, 0));
-		
-		JPanel panel_23 = new JPanel();
-		panel_23.setBackground(new Color(160, 82, 45));
-		panel_22.add(panel_23);
-		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_23.add(lblNewLabel_2);
-		
-		JPanel panel_25 = new JPanel();
-		panel_25.setBackground(new Color(160, 82, 45));
-		panel_22.add(panel_25);
-		
-		JLabel lblNewLabel_3 = new JLabel("New label");
-		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_25.add(lblNewLabel_3);
-		
-		JPanel panel_24 = new JPanel();
-		panel_24.setBackground(new Color(160, 82, 45));
-		panel_22.add(panel_24);
-		
-		JLabel lblNewLabel_4 = new JLabel("New label");
-		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_24.add(lblNewLabel_4);
-		
-		JPanel panel_26 = new JPanel();
-		panel_26.setBackground(new Color(0, 0, 139));
-		panel_8.add(panel_26, BorderLayout.CENTER);
-		panel_26.setLayout(new BorderLayout(0, 0));
 	}
 
 }
