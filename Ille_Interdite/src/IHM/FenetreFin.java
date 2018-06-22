b63a7b1515011b5addfc78c7e3a911cbfa6a313f
Zunkusem <jeremi04@hotmail.fr>
2018-06-22 16:55:19 +0200

    Modif fenetre fin

--- Ille_Interdite/src/IHM/FenetreFin.java
+++ Ille_Interdite/src/IHM/FenetreFin.java
@@ -9,31 +9,12 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import ille_intedite.Curseur;
+import javax.swing.SwingConstants;
 
 public class FenetreFin extends JFrame{
-	private JLabel labelFin;
 	public FenetreFin(MessageFinPartie msg){
 		//Si le curseur jouais la musique de fin 
 		Curseur.stopSound();
-		if (msg.isVictoire()) {
-			labelFin = new JLabel("Vous avez gagné !");
-			PlaySound.play(System.getProperty("user.dir")+"\\src\\"+"sound\\YEAAAAH.wav");
-			Font font = new Font("Serif", Font.BOLD, 25);
-			labelFin.setFont(font);
-		} else {
-			labelFin = new JLabel("Vous avez perdu...");
-			Font font = new Font("Serif", Font.BOLD, 25);
-			labelFin.setFont(font);
-			if(msg.getTypeDefaite().equalsIgnoreCase("L'ile a sombre completement..")) {
-
-			}else if(msg.getTypeDefaite().equalsIgnoreCase("L'heliport a coule..")) {
-
-			}else if(msg.getTypeDefaite().equalsIgnoreCase("Un des aventuriers s'est noye..")) {
-
-			}
-		}
-
-		JLabel label = new JLabel(msg.getTypeDefaite());
 		this.setTitle("Fin Ile Interdite");
 
 		this.setSize(500, 500);
@@ -44,29 +25,46 @@ public class FenetreFin extends JFrame{
 
 		this.setVisible(true);
 
-		this.setResizable(false);
+		this.setResizable(false)
 		setBackground(new Color(139,69,18));
 		getContentPane().setLayout(null);
 
 
 
 		PanelFont pan = new PanelFont(5,true);
-		pan.setLayout(new GridLayout(2, 1));
+		pan.setBackground(Color.WHITE);
 		pan.setBounds(0, 0, 494, 465);
 		getContentPane().add(pan);
+		pan.setLayout(null);
+		
+				JLabel labelFin = new JLabel("");
+				labelFin.setForeground(Color.WHITE);
+				labelFin.setHorizontalAlignment(SwingConstants.CENTER);
+				labelFin.setBounds(0, 100, 500, 40);
+				pan.add(labelFin);
+				
+				JLabel labelMort = new JLabel("");
+				labelMort.setForeground(Color.WHITE);
+				labelMort.setHorizontalAlignment(SwingConstants.CENTER);
+				labelMort.setBounds(0, 179, 500, 16);
+				pan.add(labelMort);
+		if (msg.isVictoire()) {
+			labelFin.setText("Vous avez gagné !");
+			PlaySound.play(System.getProperty("user.dir")+"\\src\\"+"sound\\YEAAAAH.wav");
+			Font font = new Font("Serif", Font.BOLD, 25);
+			labelFin.setFont(font);
+		} else {
+			labelFin .setText("Vous avez perdu...");
+			Font font = new Font("Serif", Font.BOLD, 25);
+			labelFin.setFont(font);
+			
+			labelMort.setText(msg.typeDefaite);
 
-		JPanel panHaut = new JPanel();
-		panHaut.setBackground(new Color(0,0,0,0));
-		JPanel panBas = new JPanel();
-		panBas.setBackground(new Color(0,0,0,0));
+			
 
-		pan.add(panHaut);
-		pan.add(panBas);
-
-		panHaut.add(labelFin);
-		panBas.add(label);
+			
+		}
+		
 
 	}
-
-
 }
