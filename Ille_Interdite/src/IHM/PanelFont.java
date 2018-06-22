51f8102723b6397933a0b05ae0a19aef6fca059d
Zunkusem <jeremi04@hotmail.fr>
2018-06-22 10:41:05 +0200

    Modif Repaint

--- Ille_Interdite/src/IHM/PanelFont.java
+++ Ille_Interdite/src/IHM/PanelFont.java
@@ -87,6 +87,8 @@ public class PanelFont extends JPanel {
 				path = System.getProperty("user.dir")+"\\src\\"+"images\\Art\\StartPanelV3.png";
 				e.printStackTrace();
 			}
+			repaint();
+			revalidate();
 		}
 		
 		if (num == 2) {
@@ -109,9 +111,10 @@ public class PanelFont extends JPanel {
 				path = System.getProperty("user.dir")+"\\src\\"+"images\\Art\\StartPanelPanel1V1.png";
 				e.printStackTrace();
 			}
+			repaint();
+			revalidate();
 		}
-		repaint();
-		revalidate();
+		
 
 
 	}
